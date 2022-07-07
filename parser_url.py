6ce0f2e3bdacf94d26f818367bfe517158c0cd2d
Nick <kersam19@yandex.ru>
2022-07-07 17:56:47 +0300

    Second commit

--- parser_url.py
+++ parser_url.py
@@ -16,13 +16,14 @@ def get_url():
             card_url = 'https://scrapingclub.com' + i.find('a').get('href')
             yield card_url
 
-for card_url in get_url():
-    response = requests.get(card_url, headers=headers)
-    sleep(3)
-    soup = BS(response.text, 'lxml')
-    data = soup.find('div', class_='card mt-4 my-4')
-    name = data.find('h3', class_='card-title').text
-    price = data.find('h4').text
-    text = data.find('p', class_='card-text').text
-    url_img = 'https://scrapingclub.com' + data.find('img', class_='card-img-top img-fluid').get('src')
-    print(name + '\n' + price + '\n' + text + '\n' + url_img + '\n\n')
+def array():
+    for card_url in get_url():
+        response = requests.get(card_url, headers=headers)
+        sleep(1)
+        soup = BS(response.text, 'lxml')
+        data = soup.find('div', class_='card mt-4 my-4')
+        name = data.find('h3', class_='card-title').text
+        price = data.find('h4').text
+        text = data.find('p', class_='card-text').text
+        url_img = 'https://scrapingclub.com' + data.find('img', class_='card-img-top img-fluid').get('src')
+        yield name, price, text, url_img
